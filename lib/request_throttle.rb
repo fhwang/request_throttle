527a09ca81139ad1cc592b6ad6a4589234eecf43
Francis Hwang <francis@diversionmedia.com>
2009-01-13 11:14:58 -0500

    refactor

--- lib/request_throttle.rb
+++ lib/request_throttle.rb
@@ -44,47 +44,51 @@ module RequestThrottle
       @base_max_req_count = base_max_req_count
     end
     
+    def decrement_memcache(cache_store, mk)
+      if RAILS_GEM_VERSION =~ /^2\.1\./ &&
+        cache_store.is_a?(ActiveSupport::Cache::MemCacheStore)
+        # dodging a bug in with MemCacheStore#decrement in Rails 2.1.x
+        Rails.cache.increment mk, -1
+      else
+        Rails.cache.decrement mk
+      end
+    end
+    
     def filter(controller)
-      too_many = false
       mk = memcache_key controller
       req_count = Rails.cache.increment(mk, 0)
       if req_count
         req_count = req_count.to_i
         if req_count >= max_req_count(controller)
-          controller.send(:render, :status => 503, :text => '')
-          too_many = true
+          controller.send(:render, :status => 503, :text => '') and return
         else
           Rails.cache.increment mk, 1
         end
       else
-        # This must start at 0 for some reason
-        Rails.cache.write(mk, '0', :expires_in => 1.month )
-        Rails.cache.increment mk, 1
+        init_req_count mk
       end
-      unless too_many
-        begin
-          yield
-        ensure
-          # This is in an ensure block because the decrement always has to
-          # happen, even if the yielded code raises an error.
+      begin
+        yield
+      ensure
+        # This is in an ensure block because the decrement always has to
+        # happen, even if the yielded code raises an error.
 
-          # There's a slim chance that the count would expire and be reset to 0
-          # by another mongrel when there's a decrement outstanding.
-          # Decrementing from zero will make memcache rollover the number to
-          # some extremely high int, so we double-check the count value before
-          # decrementing just to be sure.
-          req_count = Rails.cache.increment(mk, 0)
-          if req_count && req_count > 0
-            if RAILS_GEM_VERSION =~ /^2\.1\./ &&
-              controller.cache_store.is_a?(ActiveSupport::Cache::MemCacheStore)
-              # dodging a bug in with MemCacheStore#decrement in Rails 2.1.x
-              Rails.cache.increment mk, -1
-            else
-              Rails.cache.decrement mk
-            end
-          end
+        # There's a slim chance that the count would expire and be reset to 0
+        # by another mongrel when there's a decrement outstanding.
+        # Decrementing from zero will make memcache rollover the number to
+        # some extremely high int, so we double-check the count value before
+        # decrementing just to be sure.
+        req_count = Rails.cache.increment(mk, 0)
+        if req_count && req_count > 0
+          decrement_memcache controller.cache_store, mk
         end
       end
+    end
+    
+    def init_req_count(mk)
+      # This must start at 0 for some reason
+      Rails.cache.write(mk, '0', :expires_in => 1.month )
+      Rails.cache.increment mk, 1
     end
     
     def max_req_count(controller)
